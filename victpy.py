ec8c6ae93068d109d2fc6ac03b736cc9d23c1c2e
CEPblU1080 <109141060+CEPblU1080@users.noreply.github.com>
2022-07-12 12:18:03 +0300

    v 0

--- victpy.py
+++ victpy.py
@@ -1,3 +1,4 @@
+#dksgfsjfgksf
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton, QMessageBox
 from random import randint
